e2f5ba599d5946c165580e061c9d6f567a5f987c
Utopia <gejunwei110@gmail.com>
2020-10-24 00:08:18 +0800

    feat:剑指 Offer 06. 从尾到头打印链表

--- src/main/java/com/utopia/leetcode/codinginterviews/N6ReversePrint.java
+++ src/main/java/com/utopia/leetcode/codinginterviews/N6ReversePrint.java
@@ -1,2 +1,36 @@
-package com.utopia.leetcode.codinginterviews;public class N6ReversePrint {
+package com.utopia.leetcode.codinginterviews;
+
+/**
+ * 递归的本质就是栈
+ */
+public class N6ReversePrint {
+    int [] resArray;
+    int size;
+    int i;
+
+    public int[] reversePrint(ListNode head) {
+        recurse(head);
+        return resArray;
+    }
+
+    private void recurse(ListNode head){
+
+        if (head == null){
+            resArray = new int[size];
+            return;
+        }
+
+        size++;
+        recurse(head.next);
+        resArray[i] = head.val;
+        i++;
+
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
 }
