9a4ec499b0146bfc891ec60b88e7343bb7e6284f
Utopia <gejunwei110@gmail.com>
2020-10-23 23:23:23 +0800

    feat:剑指 Offer 05. 替换空格

---
+++ src/main/java/com/utopia/leetcode/codinginterviews/N5replaceSpace.java
@@ -0,0 +1,20 @@
+package com.utopia.leetcode.codinginterviews;
+
+public class N5replaceSpace {
+
+    public String replaceSpace01(String s) {
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (c == ' '){
+                result.append("%20");
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+}
