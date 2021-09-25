b6338fb45acf7ce531c5ef377f619c8042d78583
Utopia <gejunwei110@gmail.com>
2021-09-25 10:46:38 +0800

    剑指 Offer 45 把数组排成最小的数

---
+++ src/main/java/com/utopia/leetcode/codinginterviews/N45MinNumber.java
@@ -0,0 +1,30 @@
+package com.utopia.leetcode.codinginterviews;
+
+import java.util.Arrays;
+
+public class N45MinNumber {
+
+    public String minNumber(int[] nums) {
+
+        String[] strings = new String[nums.length];
+
+        for (int i = 0; i < nums.length; i++){
+            strings[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(strings, (o1, o2) -> (o1 + o2).compareTo(o2 + o1));
+
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < strings.length; i++){
+            stringBuffer.append(strings[i]);
+        }
+        return stringBuffer.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {30,3,34,5,9};
+
+        N45MinNumber n45MinNumber = new N45MinNumber();
+        System.out.println(n45MinNumber.minNumber(nums));
+    }
+}
